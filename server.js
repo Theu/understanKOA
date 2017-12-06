bd4161c79a979f4e673f9a27a907f6f15914302a
Matteo Contini <matteo.contini@zalando-lounge.de>
2017-12-06 17:32:57 +0100

    added router to create route to folder

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
-import Koa from 'koa'
+import Koa from 'koa';
+import Router from 'koa-router'
+import todos from './routes/todos';
 
 export const app = new Koa() //to where i export?
+
+const router = new Router();
+
+router.use('/todos', todos.routes(), todos.allowedMethods())
+app.use(todos.routes())
 app.listen(3000)
+
