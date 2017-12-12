e756abc528f20e29c57bc610a899b2136c08527c
Matteo Contini <matteo.contini@zalando-lounge.de>
2017-12-12 14:02:07 +0100

    refactored to async

--- helpers/putRequestToDataBase.js
+++ helpers/putRequestToDataBase.js
@@ -2,20 +2,19 @@ import PouchDB from 'pouchdb';
 const db = new PouchDB('newDB');
 
 const putRequestToDataBase = async ctx => {
-    const {title} = ctx.request.body
+    const {title} = ctx.request.body;
     const todo = {
         title,
         _id: new Date().toISOString(),
         isCompleted: false
     }
-    ctx.status = 201
+    ctx.status = 201;
     // PUT todo in DB
-
-    db
-        .put(todo)
-        .then(response => console.log('response', response))
-        .catch(error => console.log('error on put', error))
-
+    try {
+        return db.put(todo);
+    } catch (error)  {
+        return error;
+    };
 };
 
 export default putRequestToDataBase;
--- helpers/setBodyRequest.js
+++ helpers/setBodyRequest.js
@@ -1,26 +1,7 @@
-// TO get the body of the request
-// import db from './db';
-
-// const showTodos = function() {
-//     return db
-//                 .allDocs({
-//                     include_docs: true
-//                 })
-//                 .then(result => {
-//                     const allDocs = result.rows;
-//                     var objectEntries = Object.entries(allDocs).map(([key,value]) => {
-//                         return value.doc.title
-//                     })
-//                     return objectEntries.join();
-//                 })
-//                 .catch(error => console.log('error of showToDo', error))
-// }
 import showTodos from './showTodos';
 
-const setBodyRequest = ctx => {
-    return showTodos().then(result => {
-        ctx.body = result;
-    });
+const setBodyRequest = async ctx => {
+    ctx.body = await showTodos();
 };
 
 export default setBodyRequest;
